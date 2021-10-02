3c0128f0629570d5f32696301b589a56d58f4cb1
JoaoZati <joaozati@gmail.com>
2021-10-02 19:37:17 -0300

    - Adicionando blueprint functionality

--- flask_static.py
+++ flask_static.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
+from second import second
 
 app = Flask(__name__)
+app.register_blueprint(second, url_prefix="")
 
 
-@app.route('/home')
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return '<h1>TEST</h1>'
 
 
 if __name__ == '__main__':
