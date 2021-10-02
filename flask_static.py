dc0be71db3ce251c35b2a7e24c4c662caa1be6db
JoaoZati <joaozati@gmail.com>
2021-10-02 19:39:34 -0300

    - Adicionando url_prefix, agora só aciona o blueprint se tiver o url /admin/home

--- flask_static.py
+++ flask_static.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from second import second
 
 app = Flask(__name__)
-app.register_blueprint(second, url_prefix="")
+app.register_blueprint(second, url_prefix="/admin")
 
 
 @app.route('/')
